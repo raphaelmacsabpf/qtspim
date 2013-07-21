520bd3fb40b975210b0903fee1585ae4761b9da2
raphaelmacsabpf <raphaelmacsabpf@gmail.com>
2013-07-21 15:00:12 -0300

    Colocando o primeiro código

---
+++ main.asm
@@ -0,0 +1,11 @@
+	.data
+SSS_0:  .asciiz     "MENU\n           1 - Inserir\n           2 - Remover por indice\n           3 - Remover por valor\n           4 - Listar todos\n           5 - Sair\n"
+SSS_1:  .asciiz     "Digite o item que voce quer inserir na lista\n"
+SSS_2:  .asciiz     "--Lista Vazia\n"
+
+
+	.text
+main:
+
+	addi  $2, $0, 4937
+
